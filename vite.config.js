3e7912a36e2cd27ed442598703aa270ae556b73b
Laura Marques <lauramarques@Lauras-MacBook-Air.local>
2025-02-03 20:37:44 -0300

    Revert "updating crypto depedencies"
    This reverts commit eef51629a38ac3056f5070cad79b1622ad575d00.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react'; // If you're using React
-import nodeResolve from '@rollup/plugin-node-resolve'; // Import the plugin
+
 
 export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development/production)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    root: './src',
+    root: './src', // Set the root to the `src` folder
     server: {
-      open: true,
+      open: true, // Automatically open the browser
     },
     define: {
+      // Expose environment variables to your app
       __APP_ENV__: JSON.stringify(env.APP_ENV),
-    },
-    plugins: [
-      react(), // If you're using React
-      nodeResolve({  // Add the plugin here
-        browser: true, // Important: tells Rollup to prioritize browser-friendly versions of dependencies
-      }),
-    ],
-    resolve: {
-      alias: {
-        crypto: 'crypto-js', // This alias might not even be necessary anymore
-      },
     },
   };
 });
